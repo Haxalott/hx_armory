c59993bef9dae56ea27ec4c2a1373c3ca863e8ae
Haxalott's Developments <43390732+Haxalott@users.noreply.github.com>
2024-10-08 20:22:43 +1100

    Delete hx_armory directory

--- hx_armory/client/cl_functions.lua
+++
@@ -1,9 +0,0 @@
-function notify(title, description, type, icon)
-    lib.notify({
-        title = title,
-        description = description,
-        type = type,
-        icon = icon,
-        position = 'top-center'
-    })
-end
--- hx_armory/shared/lang.lua
+++
@@ -1,8 +0,0 @@
-L = {
-    ['REMOVE_ARMOR'] = 'Remove Armor',
-    ['ARMORY_TITLE'] = 'Armory',
-    ['WEAPON_TITLE'] = 'Weapons',
-    ['ARMOR_MENU_TITLE'] = 'Armor',
-    ['SHOP_TITLE'] = 'Shop',
-    ['OPENING_ARMORY'] = 'Open Armory'
-}
