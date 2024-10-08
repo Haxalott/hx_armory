8f060d9777c5e5f59a3e8cc23c55d6edcc2d8bdf
Haxalott's Developments <43390732+Haxalott@users.noreply.github.com>
2024-10-08 20:26:41 +1100

    Update config.lua

--- shared/config.lua
+++ shared/config.lua
@@ -9,12 +9,8 @@ Config.Armory = {
     Shop = {
         { item = 'dutyblip', label = 'Duty Blip', max = 1 },
         { item = 'ammo-40', label = '.40 S&W', max = 200 },
-        { item = 'prison_mdt', label = 'Prison Tablet', max = 1 },
-        { item = 'panicbutton', label = 'Panic Button', max = 1 },
         { item = 'handcuffs', label = 'Handcuffs', max = 2 },
-        { item = 'spikestrips', label = 'Spikestrips', max = 2 },
-        { item = 'megaphone', label = 'Megaphone', max = 1},
-        { item = 'wheelclamp', label = 'Wheelclamp', max = 2 },
+        { item = 'megaphone', label = 'Megaphone', max = 1}, -- max is the amount you can get out at once and have on you at once
         { item = 'badge', label = 'Police Badge', max = 1 }
     },
     Armor = {
@@ -23,12 +19,10 @@ Config.Armory = {
 }
 
 
-Config.SwatPassword = 'lingalonga'
+Config.SwatPassword = 'CHANGE_ME'
 Config.Swat = {
     Weapons = {
         { weapon = 'weapon_hk416', label = 'HK416 Rifle' },
-        { weapon = 'weapon_scarh', label = 'PD Scar' },
-        { weapon = 'weapon_dd16_b', label = 'DD16 Rifle' }
     },
     Shop = {
         { item = 'ammo-rifle', label = '5.56x45', max = 400 },
@@ -39,7 +33,7 @@ Config.Swat = {
     }
 }
 
-Config.Webhook = 'https://discordapp.com/api/webhooks/1238036056608079894/WpgEhPR_RYDZL3il13902kSvyZi3OH4jgtgAfsPj6guxL_-izBw9QXBJkpjinVBvv68w'
+Config.Webhook = 'CHANGE_ME'
 
 Config.Locations = {
     { location = vec3(451.97, -997.19, 29.71), type = 'armory', icon = 'fa-solid fa-gun', label = 'Open Armory' },
